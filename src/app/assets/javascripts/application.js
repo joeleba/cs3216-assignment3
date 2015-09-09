8ccd107c8ee84045535ee43b98fee17b9f0bd2a2
joeleba <hieu.giang.2810@gmail.com>
2015-09-09 23:43:24 +0800

    added requires in application.js. Rm turbolinks to prevent conflict

--- src/app/assets/javascripts/application.js
+++ src/app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require angular
+//= require angular-route
+//= require angular-resource
+//= require angular-rails-template
+
 //= require_tree .
