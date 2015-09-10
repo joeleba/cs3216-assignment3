a3282dac906da38f2d2d574ee945c59f766fffd6
joeleba <hieu.giang.2810@gmail.com>
2015-09-10 20:48:49 +0800

    remove bootstrap-sass-official from bower packages. Karen & Andhieka can add in later if you guys need.

--- src/config/application.rb
+++ src/config/application.rb
@@ -22,7 +22,7 @@ module Src
 
     # Add bower components as assets
     config.assets.paths << Rails.root.join("vendor", "assets", "bower_components")
-    config.assets.paths << Rails.root.join("vendor", "assets", "bower_components", "bootstrap-sass-official", "assets", "fonts")
+    # config.assets.paths << Rails.root.join("vendor", "assets", "bower_components", "bootstrap-sass-official", "assets", "fonts")
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
