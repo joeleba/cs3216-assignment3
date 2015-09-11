163499a8966fca3c0a7798f7456691a1420aa6ca
joeleba <hieu.giang.2810@gmail.com>
2015-09-11 17:10:51 +0800

    rough api for services and stops controllers

--- src/app/controllers/api/v1/services_controller.rb
+++ src/app/controllers/api/v1/services_controller.rb
@@ -1,3 +1,7 @@
+# WARNING:
+# Params are not finalized. Will need further discussion
+# I/O Documentations will be added later
+
 module Api
   module V1
     class ServicesController < ApplicationController
@@ -5,14 +9,22 @@ module Api
 
       # GET /api/services
       def index
+        @services = Service.all
+        respond_with @services
       end
 
       # GET /api/services/:id
       def show
+        @service = Service.find(params[:id])
+        respond_with @service
       end
 
       # GET /api/services/:id/stops
       def stops
+        @service = Service.find(params[:id])
+        @stops = @services.stops
+
+        respond_with @stops
       end
     end
   end
--- src/app/controllers/api/v1/stops_controller.rb
+++ src/app/controllers/api/v1/stops_controller.rb
@@ -1,3 +1,7 @@
+# WARNING:
+# Params are not finalized. Will need further discussion
+# I/O Documentations will be added later
+
 module Api
   module V1
     class StopsController < ApplicationController
@@ -5,14 +9,21 @@ module Api
 
       # GET /api/stops
       def index
+        @stops = Stop.all
+        respond_with @stops
       end
 
       # GET /api/stops/:id
       def show
+        @stop = Stop.find(params[:id])
+        respond_with @stop
       end
 
       # GET /api/stops/:id/services
       def services
+        @stop = Stop.find(params[:id])
+        @services = @stop.services
+        respond_with @services
       end
     end
   end
