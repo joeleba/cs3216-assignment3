6e43a103dcf55a1b5d11846cdd2f8e572b3058ee
joeleba <hieu.giang.2810@gmail.com>
2015-09-11 15:19:39 +0800

    add resources to routes

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  namespace :api do
+    resources :services, :stops, :users
+  end
 end
