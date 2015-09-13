ebc3da2753583d0419a035c90773f71aafe9e876
thamzhengyi <thamzhengyi@gmail.com>
2015-09-13 21:19:19 +0800

    Refactored the logic for initializing the report form into a named function.

--- src/app/assets/javascripts/angular/controllers/reportController.js
+++ src/app/assets/javascripts/angular/controllers/reportController.js
@@ -7,18 +7,22 @@ function ReportController($scope, $http) {
   $scope.busTypes = ['A1', 'A2', 'B', 'C', 'D1', 'D2', 'UT-CLB'];
   $scope.fullnessLevels = ['empty', 'ok lah', 'full'];
   $scope.submitReport = function(busType, fullnessLevel) {
-    alert('submit');
+    alert('submitting bus type: ' + $("#bus-type").val() + ' and fullness level: ' + $("#fullness-level").val());
     //$http.post('path', { busType: $("#bus-type").val(), fullessLevel: $("#fullness-level").val() });
   }
 
-  $(document).ready(function() {
+  $(document).ready(initializeReportForm);
+
+  function initializeReportForm() {
+    $(".bus-type").css('color', 'black');
     var firstBusType = $(".bus-type").first();
     firstBusType.css('color', 'green');
     $("#bus-type").val(firstBusType.text());
 
+    $(".fullness-level").css('color', 'black');
     var firstFullnessLevel = $(".fullness-level").first();
     firstFullnessLevel.css('color', 'green');
     $("#fullness-level").val(firstFullnessLevel.text());
-  });
+  }
 }
 })();
