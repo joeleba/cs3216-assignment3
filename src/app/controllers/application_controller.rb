701085883cc01d16e0537bc5cbe635ad061a1562
Andhieka Putra <putra@garena.com>
2015-09-14 11:28:40 +0800

    Integrate UI with Facebook Auth

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
-    # def check_logged_in
-    #   unless @current_user
-    #     redirect_to root_url
-    #   end
-    # end
+    def check_logged_in
+      unless @current_user
+        redirect_to '#'
+      end
+    end
 end
--- src/app/controllers/sessions_controller.rb
+++ src/app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
     omniauth_hash = env['omniauth.auth']
     user = User.from_omniauth(omniauth_hash)
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_to '#/main'
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '#{root_url}'
+    redirect_to '#'
   end
 
 end
