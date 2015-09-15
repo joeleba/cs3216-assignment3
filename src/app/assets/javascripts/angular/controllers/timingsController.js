a11d36c0bcedb7865dd0bbe68e2808b55ba649af
Karen Ang <thekarenang@gmail.com>
2015-09-15 21:30:43 +0800

    Improve styling and UX for the schedule page

--- src/app/assets/javascripts/angular/controllers/timingsController.js
+++ src/app/assets/javascripts/angular/controllers/timingsController.js
@@ -4,12 +4,15 @@
   .controller('TimingsController', ["$scope", TimingsController]);
 
 function TimingsController($scope) {
-  $scope.busData = [{name: 'A1', capacity: 'full', arrivalTiming: '20 min', lastSeen: '5m ago at YIH', showArrivalTiming: false},
-                    {name: 'A2', capacity: 'full', arrivalTiming: '20 min', lastSeen: '10m ago at Museum', showArrivalTiming: false},
-                    {name: 'B', capacity: 'empty', arrivalTiming: '1 min', lastSeen: '3m ago at EA', showArrivalTiming: false},
-                    {name: 'C', capacity: 'empty', arrivalTiming: '2 min', lastSeen: '1m ago at PGP', showArrivalTiming: false},
-                    {name: 'D1', capacity: 'average', arrivalTiming: '10 min', lastSeen: '', showArrivalTiming: false, direction: 'UTown to Biz'},
-                    {name: 'D2', capacity: 'full', arrivalTiming: '6 min', lastSeen: '', showArrivalTiming: false},
-                    {name: 'UT-CLB', capacity: 'average', arrivalTiming: '2 min', lastSeen: '2m ago at CLB', showArrivalTiming: false}];
+  $scope.busData = [{name: 'A1', capacity: 'full', arrivalTiming: '20m', lastSeenTime: '1600', lastSeenLoc: 'YIH', showDetail: false},
+                    {name: 'A2', capacity: 'full', arrivalTiming: '20m', lastSeenTime: '1605', lastSeenLoc: 'Museum', showDetail: false},
+                    {name: 'B', capacity: 'empty', arrivalTiming: '1m', lastSeenTime: '1556', lastSeenLoc: 'EA', showDetail: false},
+                    {name: 'C', capacity: 'empty', arrivalTiming: '2m', lastSeenTime: '1601', lastSeenLoc: 'UHall', showDetail: false},
+                    {name: 'D1', capacity: 'average', arrivalTiming: '10m', lastSeenTime: '1550', lastSeenLoc: 'UT', showDetail: false, direction: 'UTown to Biz'},
+                    {name: 'D2', capacity: 'full', arrivalTiming: '6m', lastSeenTime: '1540', lastSeenLoc: 'Opp. UHall', showDetail: false},
+                    {name: 'UT-CLB', capacity: 'average', arrivalTiming: '2m', lastSeenTime: '1557', lastSeenLoc: 'CLB', showDetail: false}];
+  $scope.toggleDetail = function(bus) {
+    bus.showDetail = bus.showDetail === false ? true : false;
+  }
 }
 })();
