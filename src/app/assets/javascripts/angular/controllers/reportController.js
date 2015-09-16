c09aa5ad86fffa998d27c8c7d62185dba9a98ee7
thamzhengyi <thamzhengyi@gmail.com>
2015-09-16 16:58:27 +0800

    Adjusted fullness level name to be less confusing.

--- src/app/assets/javascripts/angular/controllers/reportController.js
+++ src/app/assets/javascripts/angular/controllers/reportController.js
@@ -5,7 +5,7 @@
 
 function ReportController($scope, $http) {
   $scope.busTypes = ['A1', 'A2', 'B', 'C', 'D1', 'D2', 'UT-CLB'];
-  $scope.fullnessLevels = ['empty', 'ok lah', 'full'];
+  $scope.fullnessLevels = ['empty', 'half full', 'full'];
   $scope.submitReport = function(busType, fullnessLevel) {
     alert('submitting bus type: ' + $("#bus-type").val() + ' and fullness level: ' + $("#fullness-level").val());
     //$http.post('path', { busType: $("#bus-type").val(), fullessLevel: $("#fullness-level").val() });
