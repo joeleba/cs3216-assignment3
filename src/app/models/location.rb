5d96b82d238861423ca4456d66abf8829047d691
joeleba <hieu.giang.2810@gmail.com>
2015-09-18 01:04:39 +0800

    give front end full obj instead

--- src/app/models/location.rb
+++ src/app/models/location.rb
@@ -4,21 +4,21 @@ class Location
     stops_array = []
     get_all_stops_coord.each { |stop_obj|
       stop_array_entry = {}
-      stop_array_entry[:name] = stop_obj[:name]
+      stop_array_entry[:stop] = stop_obj[:stop]
       stop_array_entry[:distance] = Geocoder::Calculations.distance_between(userLocation, stop_obj[:coord])
       stops_array.push(stop_array_entry)
     }
-    stops_array.sort_by! {|obj| obj[:distance]}
+    { nearby_stops: stops_array.sort_by! {|obj| obj[:distance]} }
   end
 
   private
 
   def self.get_all_stops_coord
     coords = []
-    stops = Stops.all
+    stops = Stop.all
     stops.each { |stop|
       obj = {}
-      obj[:name] = stop.name
+      obj[:stop] = stop
       obj[:coord] = [stop.latitude, stop.longitude]
       coords.push(obj)
     }
