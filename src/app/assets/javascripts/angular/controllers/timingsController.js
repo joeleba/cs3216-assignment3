76647f514bd33babdc09da962b2d4fd010ccb94a
Karen Ang <thekarenang@gmail.com>
2015-09-17 19:36:20 +0800

    Integrate TimingsController with back end API 🚌

--- src/app/assets/javascripts/angular/controllers/timingsController.js
+++ src/app/assets/javascripts/angular/controllers/timingsController.js
@@ -1,22 +1,86 @@
 (function() {
   angular
   .module('nexbus')
-  .controller('TimingsController', ["$scope", TimingsController]);
+  .controller('TimingsController', ["$scope", "$http", "$route", "$location",
+  "$timeout", TimingsController]);
 
-function TimingsController($scope) {
-  $scope.busData = [{name: 'A1', capacity: 'full', arrivalTiming: '20m', lastSeenTime: '1600', lastSeenLoc: 'YIH', showDetail: false},
-                    {name: 'A2', capacity: 'full', arrivalTiming: '20m', lastSeenTime: '1605', lastSeenLoc: 'Museum', showDetail: false},
-                    {name: 'B', capacity: 'empty', arrivalTiming: '1m', lastSeenTime: '1556', lastSeenLoc: 'EA', showDetail: false},
-                    {name: 'C', capacity: 'empty', arrivalTiming: '2m', lastSeenTime: '1601', lastSeenLoc: 'UHall', showDetail: false},
-                    {name: 'D1', capacity: 'average', arrivalTiming: '10m', lastSeenTime: '1550', lastSeenLoc: 'UT', showDetail: false, direction: 'UTown to Biz'},
-                    {name: 'D2', capacity: 'full', arrivalTiming: '6m', lastSeenTime: '1540', lastSeenLoc: 'Opp. UHall', showDetail: false},
-                    {name: 'UT-CLB', capacity: 'average', arrivalTiming: '2m', lastSeenTime: '1557', lastSeenLoc: 'CLB', showDetail: false}];
-  $scope.toggleDetail = function(bus) {
-    bus.showDetail = bus.showDetail === false ? true : false;
+function TimingsController($scope, $http, $route, $location, $timeout) {
+
+  var params = $route.current.params;
+
+  // Returns the selected Stop name given the stop_id
+  $scope.getStopName = function(stop_id) {
+    $http.get('/api/v1/stops/' + stop_id).
+      then(function(res) {
+        $scope.currentStopName = res.data.name;
+      }, function(err) {
+        $scope.currentStopName = '';
+        $scope.error = err;
+      })
   };
+
+  // Returns all the Services available at a particular Stop
+  $scope.getServicesAt = function(stop_id) {
+    $http.get('/api/v1/stops/' + stop_id + '/services').
+      then(function(res) {
+        $scope.serviceData = res.data;
+        getSightingsForServices(params.stop_id, $scope.serviceData);
+      }, function(err) {
+        $scope.serviceData = {};
+        $scope.error = err;
+      });
+  };
+
+  // Retrieves the Sighting data for the Services specified
+  function getSightingsForServices(stop_id, services) {
+    $scope.sightingsData = [];
+    for (var i=0; i<services.length; i++) {
+      var svc = services[i];
+      getSighting(params.stop_id, svc.id, svc.name);
+      $timeout(()=> { $scope.refreshSchedule = false; }, 1500);
+    }
+  };
+
+  // Returns the Sighting data for a particular Service
+  function getSighting(stop_id, service_id, service_name) {
+    $http.get('/api/v1/sightings?service_id=' + service_id + '&stop_id=' +
+    stop_id).
+      then(function(res) {
+      var busData = {};
+      var detail = res.data.prev_stops.status;
+      var lastSeen;
+
+      if (res.data.last_seen) {
+        lastSeen = res.data.prev_stops.stop.name + ' | ' + res.data.last_seen;
+      } else {
+        lastSeen = 'No data'
+      }
+      // This is quite messy and should be refactored in the future
+      busData["name"] = service_name;
+      busData["thisLastSeen"] = res.data.this_stop;
+      busData["otherLastSeen"] = lastSeen
+      busData["detail"] = detail === "" ? false : true;
+      busData["capacity"] = detail;
+      $scope.sightingsData.push(busData);
+    }, function(err) {
+      $scope.error = err;
+    });
+  };
+
+  $scope.toggleDetail = function(bus) {
+    bus.detail = bus.detail === false ? true : false;
+  };
+
   $scope.refreshSchedule = false;
+
   $scope.toggleRefresh = function() {
-    $scope.refreshSchedule = $scope.refreshSchedule === false ? true : false;
+    $scope.refreshSchedule = true;
+    getSightingsForServices(params.stop_id, $scope.serviceData);
+  }
+
+  if ($location.path() === '/main') {
+    $scope.getStopName(params.stop_id);
+    $scope.getServicesAt(params.stop_id);
   }
 }
 })();
