49f84ca2c81b7fdd9024fafaac1b7338a6b3af74
joeleba <hieu.giang.2810@gmail.com>
2015-09-18 01:04:48 +0800

    documentation

--- src/app/controllers/api/v1/locations_controller.rb
+++ src/app/controllers/api/v1/locations_controller.rb
@@ -1,14 +1,35 @@
-# WARNING:
-# Params are not finalized. Will need further discussion
-# I/O Documentations will be added later
-
 module Api
   module V1
     class LocationsController < ApplicationController
       respond_to :json
 
       # GET /api/v1/locations?lat=&lon=
-      # Get last seen information of a service.
+      #
+      # Get an array of nearby stops, ranked by distance ASC
+      # Params: lat & lon
+      # Return value:
+      # {
+      #   "nearby_stops": [
+      #     {
+      #       "stop": {
+      #         "id": 10,
+      #         "name": "COM2",
+      #         "longitude": "103.7736903995500000",
+      #         "latitude": "1.2943139822027000"
+      #       },
+      #       "distance": 0.04430261437480384
+      #     },
+      #     {
+      #       "stop": {
+      #         "id": 11,
+      #         "name": "BIZ2",
+      #         "longitude": "103.7751080857900000",
+      #         "latitude": "1.2936685240305000"
+      #       },
+      #       "distance": 0.09140004095787715
+      #     },
+      #     ...]
+      # }
       def nearby
         render json: Location.get_nearby(params)
       end
