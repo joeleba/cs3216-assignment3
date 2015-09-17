34826b19d83039543419a604d2cf7a0c0fc87062
thamzhengyi <thamzhengyi@gmail.com>
2015-09-17 22:13:08 +0800

    Switched to getting the actual services of the stop on the report page instead of using placeholders.

--- src/app/assets/javascripts/angular/controllers/reportController.js
+++ src/app/assets/javascripts/angular/controllers/reportController.js
@@ -1,10 +1,9 @@
 (function() {
   angular
   .module('nexbus')
-  .controller('ReportController', ["$scope", "$http", ReportController]);
+  .controller('ReportController', ["$scope", "$http", "$route", "$location", ReportController]);
 
-function ReportController($scope, $http) {
-  $scope.busTypes = ['A1', 'A2', 'B', 'C', 'D1', 'D2', 'UT-CLB'];
+function ReportController($scope, $http, $route, $location) {
   $scope.fullnessLevels = ['empty', 'half full', 'full'];
   $scope.submitReport = function(busType, fullnessLevel) {
     alert('submitting bus type: ' + $("#bus-type").val() + ' and fullness level: ' + $("#fullness-level").val());
@@ -12,6 +11,35 @@ function ReportController($scope, $http) {
     //$http.post('submitPath', { busType: $("#bus-type").val(), fullessLevel: $("#fullness-level").val() });
     initializeReportForm();
     $(".alert-block").text('Thank you for your submission!');
+  }
+
+  var params = $route.current.params;
+
+  // Returns the selected Stop name given the stop_id
+  $scope.getStopName = function(stop_id) {
+    $http.get('/api/v1/stops/' + stop_id).
+      then(function(res) {
+        $scope.currentStopName = res.data.name;
+      }, function(err) {
+        $scope.currentStopName = '';
+        $scope.error = err;
+      })
+  };
+
+  // Returns all the Services available at a particular Stop
+  $scope.getServicesAt = function(stop_id) {
+    $http.get('/api/v1/stops/' + stop_id + '/services').
+      then(function(res) {
+        $scope.serviceData = res.data;
+      }, function(err) {
+        $scope.serviceData = {};
+        $scope.error = err;
+      });
+  };
+
+  if ($location.path() === '/main') {
+    $scope.getStopName(params.stop_id);
+    $scope.getServicesAt(params.stop_id);
   }
 
   $(window).load(initializeReportForm);
