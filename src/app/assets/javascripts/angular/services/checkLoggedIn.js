16b9236c582ed5444fb99a0d008777ae4e7b7559
joeleba <hieu.giang.2810@gmail.com>
2015-09-19 00:34:11 +0800

    checkLoggedIn service

--- src/app/assets/javascripts/angular/services/checkLoggedIn.js
+++ src/app/assets/javascripts/angular/services/checkLoggedIn.js
@@ -1,18 +1,24 @@
 (function() {
   angular
     .module('nexbus')
-    .factory('loggedIn', function($q, $http) {
+    .factory('checkLoggedIn', function($q, $http) {
       return function() {
         var deferred = $q.defer();
 
         $http.get('/auth/signed_in').
           then(function(res) {
             if (res) {
-              deferred.resolve();
+              console.log("Signed In");
+              console.log(res);
+              deferred.resolve("Signed In");
             } else {
-              deferred.reject();
+              console.log("not signed in");
+              console.log(res);
+              deferred.reject("Not Signed In");
             }
-          }, function(err) {});
+          }, function(err) {
+            console.log(err);
+          });
       }
     });
 })();
