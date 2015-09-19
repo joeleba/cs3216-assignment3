c2dbc57b9c94b7926b54cfc63c8b5de24450e124
Karen Ang <thekarenang@gmail.com>
2015-09-19 16:10:54 +0800

    Fix facebook authentication; Hack again; Time lost (╯°□°)╯︵ ┻━┻

--- src/app/assets/javascripts/angular/controllers/loginController.js
+++ src/app/assets/javascripts/angular/controllers/loginController.js
@@ -1,9 +1,13 @@
 (function() {
   angular
   .module('nexbus')
-  .controller('LoginController', ["$scope", LoginController]);
+  .controller('LoginController', ["$scope", "$window", LoginController]);
 
-function LoginController($scope) {
+function LoginController($scope, $window) {
+  $scope.facebookAuth = function() {
+    // Hack due to push.js and angularjs compatibility issues
+    $window.location.href = '/auth/facebook';
+  }
 
 }
 })();
