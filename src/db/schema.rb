d019098a6be23bdbbe9179b96b7ce8b1a7ad313b
Karen Ang <thekarenang@gmail.com>
2015-09-22 04:33:43 +0800

    Add `title` to `Users`

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910141712) do
+ActiveRecord::Schema.define(version: 20150921201538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,14 +50,15 @@ ActiveRecord::Schema.define(version: 20150910141712) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer  "credit",           null: false
+    t.integer  "credit",                       null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.integer  "title",            default: 0, null: false
   end
 
   add_foreign_key "timings", "services"
