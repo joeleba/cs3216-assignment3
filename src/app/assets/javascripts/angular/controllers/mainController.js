8cda77a892dc637db135db2628096e55ceb2ce59
Karen Ang <thekarenang@gmail.com>
2015-09-22 13:10:41 +0800

    Remove console.log

--- src/app/assets/javascripts/angular/controllers/mainController.js
+++ src/app/assets/javascripts/angular/controllers/mainController.js
@@ -7,14 +7,12 @@ function MainController($scope, $location, $http) {
       $http.get('/auth/signed_in').
         then(function(res) {
           if (res.data.user !== null) {
-            console.log('logged in');
             $location.path('/location');
           } else {
-            console.log('not logged in');
             $location.path('/login');
           }
         }, function(err) {
-          console.log(err);
+          $scope.error = err;
         });
     }
 
