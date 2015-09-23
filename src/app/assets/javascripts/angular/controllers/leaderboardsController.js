32ffa1869fe5264622c486b5df69220b3ea67cc0
Karen Ang <thekarenang@gmail.com>
2015-09-24 01:13:42 +0800

    Add refresh button for all tabs

--- src/app/assets/javascripts/angular/controllers/leaderboardsController.js
+++ src/app/assets/javascripts/angular/controllers/leaderboardsController.js
@@ -3,20 +3,27 @@
   .module('nexbus')
   .controller('LeaderboardsController',
 
-  ["$scope", "$http", "$location", "$window", "$sessionStorage", LeaderboardsController]);
+  ["$scope", "$http", "$location", "$window", "$sessionStorage", "$timeout", LeaderboardsController]);
 
-  function LeaderboardsController($scope, $http, $location, $window, $sessionStorage) {
+  function LeaderboardsController($scope, $http, $location, $window, $sessionStorage, $timeout) {
     $scope.page = $location.path();
     $scope.leaderboard = [];
+    $scope.clientLoading = false;
 
     function getLeaderboard() {
       $http.get('/api/v1/users').
       then(function(res) {
+        $timeout(function() { $scope.clientLoading = false; }, 500);
         $scope.leaderboard = res.data;
         $sessionStorage.leaderboardUsers = $scope.leaderboard;
       }, function (err) {
         $scope.error = err;
       });
+    }
+
+    $scope.clientGetLeaderboard = function () {
+      $scope.clientLoading = true;
+      getLeaderboard();
     }
 
     // Workaround because push.js doesn't seem to work well with the fragment identifier
