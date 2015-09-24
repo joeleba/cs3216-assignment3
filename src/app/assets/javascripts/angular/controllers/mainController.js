5afebd14af8dae6648fc835c5294da96beb0278d
joeleba <hieu.giang.2810@gmail.com>
2015-09-24 16:16:08 +0800

    minor changes in controllers

--- src/app/assets/javascripts/angular/controllers/mainController.js
+++ src/app/assets/javascripts/angular/controllers/mainController.js
@@ -4,16 +4,5 @@
   .controller('MainController', ["$scope", "$location", "$http", MainController]);
 
 function MainController($scope, $location, $http) {
-      $http.get('/auth/signed_in').
-        then(function(res) {
-          if (res.data.user !== null) {
-            $location.path('/location');
-          } else {
-            $location.path('/login');
-          }
-        }, function(err) {
-          $scope.error = err;
-        });
-    }
 
-})();
+}})();
