36e4bec183e05e689bafc4572f414f340ab7afe2
joeleba <hieu.giang.2810@gmail.com>
2015-09-24 17:32:19 +0800

    trying something here

--- src/app/assets/javascripts/angular/controllers/mainController.js
+++ src/app/assets/javascripts/angular/controllers/mainController.js
@@ -4,5 +4,16 @@
   .controller('MainController', ["$scope", "$location", "$http", MainController]);
 
 function MainController($scope, $location, $http) {
-
+  $http.get('/auth/signed_in').
+    then(function(res) {
+      if (res.data.user !== null) {
+        console.log('main signed in');
+        $location.path('/location');
+      } else {
+        console.log('main not signed in');
+        $location.path('/login');
+      }
+    }, function(err) {
+      $scope.error = err;
+    });
 }})();
