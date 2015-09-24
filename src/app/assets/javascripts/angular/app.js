1de3fb3c6ec9909fa2f8ecc0911891fdf267d400
Karen Ang <thekarenang@gmail.com>
2015-09-24 20:48:02 +0800

    Implement Offline/Online sync with AsyncQ

--- src/app/assets/javascripts/angular/app.js
+++ src/app/assets/javascripts/angular/app.js
@@ -69,4 +69,50 @@
       templateUrl: 'loading.html'
     };
   });
+  app.run(['$window', '$rootScope', 'asyncQ', '$sessionStorage', '$interval', function($window, $rootScope, asyncQ, $sessionStorage, $interval) {
+
+    $rootScope.postOfflineSightings = function () {
+      if (typeof $sessionStorage.cachedSightings !== 'undefined') {
+        asyncQ.postOfflineSightings()
+              .then(function(res) {
+                alert(res.message);
+                if ($rootScope.hasInterval) {
+                  $interval.cancel($rootScope.interval);
+                }
+              }, function(reason) {
+                if ($rootScope.hasInterval === false) {
+                  $rootScope.initiateRetry();
+                }
+              });
+      } else {
+        if ($rootScope.hasInterval) {
+          $interval.cancel($rootScope.interval);
+        }
+      }
+    }
+
+    $rootScope.initiateRetry = function () {
+      $rootScope.interval = $interval($rootScope.postOfflineSightings(), 60000, 10)
+                            .then(function(res) {
+
+                            }, function(err) {
+                              var errors = $sessionStorage.cachedSightings.length;
+                              delete $sessionStorage.cachedSightings;
+                              alert('Oops, unable to submit ' + errors + ' sightings submitted offline. Sorry!');
+                            });
+    }
+
+    $rootScope.hasInterval = function () {
+      return typeof $rootScope.interval !== 'undefined';
+    }
+
+    $window.addEventListener('offline', function(event) {
+      $rootScope.shouldQueue = true;
+    }, false);
+
+    $window.addEventListener('online', function(event) {
+      $rootScope.shouldQueue = false;
+      $rootScope.postOfflineSightings();
+    }, false);
+  }]);
 })();
